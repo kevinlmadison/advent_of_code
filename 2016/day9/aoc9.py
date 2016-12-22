4852d5a72668f9ecd077e74b7cf753e0ba0d5d7e
kevinlmadison <coolklm121@gmail.com>
2016-12-21 20:22:41 -0500

    solved day 9

--- 2016/day9/aoc9.py
+++ 2016/day9/aoc9.py
@@ -14,11 +14,26 @@ def part1(line):
         buffer += add * mult
         line = line[marker.end() + num:]
     buffer += line
-    return len(buffer.strip())
+    return len(buffer)
 
+def part2(line):
+    buffer = 0
+    while line:
+        marker = re.match(r'(.*?)\((\d+)x(\d+)\)', line)
+        if not marker:
+            break
+        text, num, mult = marker.groups()
+        buffer += len(text)
+        num, mult = int(num), int(mult)
+        add = part2(line[marker.end():marker.end() + num])
+        buffer += add * mult
+        line = line[marker.end() + num:]
+    buffer += len(line)
+    return buffer
 
 if __name__ == '__main__':
     with open('input.txt', 'r') as file:
         line = next(file).strip()
         print(part1(line))
+        print(part2(line))
 
