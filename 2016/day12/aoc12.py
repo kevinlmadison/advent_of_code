3d0c75afbaf0054708eb829e57e2e6f21edb8c54
kevinlmadison <coolklm121@gmail.com>
2017-05-14 02:41:49 -0400

    finished day 12

--- 2016/day12/aoc12.py
+++ 2016/day12/aoc12.py
@@ -1,11 +1,47 @@
-import re
-
 # part 1
 
 
-class Registry:
-    def __init__(self):
-        self.regs = {'a': 0, 'b': 0, 'c': 0, 'd': 0}
+def reggies(file):
+    reg = {'a': 0, 'b': 0, 'c': 1, 'd': 0}
+    inst = []
+    for line in file:
+        inst.append(line)
+    end = len(inst)
+    i = 0
+    print(inst[i].strip().split(' '))
+    while i < end:
+        move = inst[i].strip().split(' ')
+        print(move)
+        if move[0] == 'cpy':
+            if move[1] not in 'abcd':
+                reg[move[2]] = int(move[1])
+            else:
+                reg[move[2]] = reg[move[1]]
+            i += 1
+        if move[0] == 'inc':
+            reg[move[1]] += 1
+            i += 1
+        if move[0] == 'dec':
+            reg[move[1]] -= 1
+            i += 1
+        if move[0] == 'jnz':
+            if move[1] not in 'abcd':
+                if int(move[1]) != 0:
+                    i += int(move[2])
+                else:
+                    i += 1
+            elif reg[move[1]] != 0:
+                i += int(move[2])
+            else: 
+                i += 1
+        print(reg)
+    return reg
 
-    def copy(self, val, reg):
-        self.regs[reg] = val
+def main():
+    f = open('input.txt')
+    a = reggies(f)
+    print(a)
+
+if __name__ == '__main__':
+    main()
+
