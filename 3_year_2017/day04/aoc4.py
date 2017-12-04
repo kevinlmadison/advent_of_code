61705db93f613106e6854799d8c6d5584fae036f
kevinlmadison <coolklm121@gmail.com>
2017-12-04 08:58:16 -0500

    made day 4 solutions much cleaner, more functional

--- 3_year_2017/day04/aoc4.py
+++ 3_year_2017/day04/aoc4.py
@@ -1,63 +1,16 @@
+
 def part1(f):
-    valid = 0
-    for line in f:
-        line = line.split()
-        s = []
-        val = True
-        for word in line:
-            if word in s:
-                val = False
-                break
-            else:
-                s.append(word)
-        if val:
-            valid += 1
-    print(valid)
-
+    print(str(sum((1 if len(w.split()) == len(set(w.split())) else 0) for w in f)))
+    
 def part2(f):
-    valid = 0
-    for line in f:
-        valid += 1
-        line = line.split()
-        val = True
-        for j,word1 in enumerate(line):
-            for i,word2 in enumerate(line):
-                if j != i and anagramSolution1(word1, word2) == True and len(word1) == len(word2):
-                    valid -= 1
-                    print('{} {}: count = {}'.format(word1, word2, valid))
-                    val = False
-                    break
-            if val == False:
-                break
-    print(str(valid))
+    w = [[''.join(sorted(i)) for i in line.split()] for line in f]
+    print(str(sum((1 if len(x) == len(set(x)) else 0) for x in w)))
 
-def anagramSolution1(s1,s2):
-    alist = list(s2)
-
-    pos1 = 0
-    stillOK = True
-
-    while pos1 < len(s1) and stillOK:
-        pos2 = 0
-        found = False
-        while pos2 < len(alist) and not found:
-            if s1[pos1] == alist[pos2]:
-                found = True
-            else:
-                pos2 = pos2 + 1
-
-        if found:
-            alist[pos2] = None
-        else:
-            stillOK = False
-
-        pos1 = pos1 + 1
-
-    return stillOK   
 
 def main():
     f = open('input.txt')
-    #part1(f)
+    part1(f)
+    f = open('input.txt')
     part2(f)
 
 if __name__ == '__main__':
