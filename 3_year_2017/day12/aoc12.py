22b09b46a0d06750a9ec1756d78157dbe2a0428a
kevinlmadison <coolklm121@gmail.com>
2017-12-12 11:49:44 -0500

    day 12 clean bfs

---
+++ 3_year_2017/day12/aoc12.py
@@ -0,0 +1,47 @@
+from collections import defaultdict, deque
+
+def solve(lines):
+
+    visited = list()
+    groups = 0
+    d = defaultdict(list)
+    for line in lines:
+        d[int(line[0])] = [int(x) for x in line[2:]]
+
+    '''Part 1 of this challenge can be solved using breadth or depth first
+    search and starting at node 0.
+    '''
+
+    def bfs(seed):
+        v = list()
+        q, count = deque([seed]), 0
+        while q:
+            x = q.popleft()
+            v.append(x)
+            count += 1
+            for i in d[x]:
+                if i not in v:
+                    q.append(i) 
+        return v
+
+    '''Since the input adjacency list represents an undirected graph we know 
+    that any one node can only belong to one group, so we only perform additional
+    best first searches starting from nodes we have not yet visited.
+    '''
+
+    for i in range(len(lines)):
+        if i not in visited:
+            visited.extend(bfs(i))
+            groups += 1
+        if i == 0:
+            print('part 1: {}'.format(len(visited)))
+    print('part 2: {}'.format(groups))
+
+def main():
+    f = open('input.txt').readlines()
+    '''No need for regex here.'''
+    f = [x.replace(',','').split() for x in f]
+    solve(f)
+
+if __name__ == '__main__':
+    main()
