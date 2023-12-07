2cf54e17af2dc57962d196d7e363a1e246db12d1
kevinlmadison <coolklm121@gmail.com>
2023-12-06 23:03:28 -0500

    amazing python code

--- 9_year_2023/day05/solution.py
+++ 9_year_2023/day05/solution.py
@@ -1,6 +1,5 @@
 import re
-import math
-from collections import Counter
+from multiprocessing import Process, Queue
 
 def part1(inputs: list[str]) -> int:
     seeds = [[] for _ in range(8)]
@@ -53,11 +52,52 @@ def part2(inputs):
         for (x, y) in seed_ranges:
             if x <= i <= y:
                 return rv
+######## I DIDN'T WRITE THIS, IT'S TO LEARN FROM ###############
+def pairs(l):
+    it = iter(l)
+    return zip(it, it)
+
+def p2(f):
+    seeds, *mappings = f.read().split("\n\n")
+    seeds = seeds.split(": ")[1]
+    seeds = [int(x) for x in seeds.split()]
+    seeds = [range(a, a + b) for a, b in pairs(seeds)]
+
+    for m in mappings:
+        _, *ranges = m.splitlines()
+        ranges = [[int(x) for x in r.split()] for r in ranges]
+        ranges = [(range(a, a + c), range(b, b + c)) for a, b, c in ranges]
+        new_seeds = []
+
+        for r in seeds:
+            for tr, fr in ranges:
+                offset = tr.start - fr.start
+                if r.stop <= fr.start or fr.stop <= r.start:
+                    continue
+                ir = range(max(r.start, fr.start), min(r.stop, fr.stop))
+                lr = range(r.start, ir.start)
+                rr = range(ir.stop, r.stop)
+                if lr:
+                    seeds.append(lr)
+                if rr:
+                    seeds.append(rr)
+                new_seeds.append(range(ir.start + offset, ir.stop + offset))
+                break
+            else:
+                new_seeds.append(r)
+
+        seeds = new_seeds
+
+    return min(x.start for x in seeds)
+######## I DIDN'T WRITE THIS, IT'S TO LEARN FROM ###############
+
 def main():
     # inputs_1 = list(open('input').readlines())
-    inputs_2 = list(open('input').readlines())
+    # inputs_2 = list(open('input').readlines())
     # print(f'part 1: {part1(inputs_1)}')
-    print(f'part 2: {part2(inputs_2)}')
+    # print(f'part 2: {part2(inputs_2)}')
+    f = open('input')
+    print(f'test p2: {p2(f)}')
 
 if __name__ == '__main__':
     main()
