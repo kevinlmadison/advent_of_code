2f6c74caecf0f5928fde3c915f44fea3bed966ac
kevinlmadison <coolklm121@gmail.com>
2023-12-28 17:16:24 -0500

    starting day 12

--- 9_year_2023/day10/solution.py
+++ 9_year_2023/day10/solution.py
@@ -1,20 +1,83 @@
-import re
-import math
+#!/usr/bin/env pypy3
+from collections import deque
 
-def process(line):
-    if not any(line):
-        return 0
-    m = [b - a for a, b in zip(line, line[1:])]
-    return line[-1] + process(m)
+class _(tuple):
 
+    def __add__(self, other):
+        return _(x + y for x,y in zip(self, other))
+
+    def __neg__(self):
+        return _(-x for x in self)
+
+ADJ = {
+    '.': [],
+    '|': [(1, 0), (-1,0)],
+    '-': [(0, 1), (0,-1)],
+    'F': [(1, 0), (0, 1)],
+    '7': [(1, 0), (0,-1)],
+    'L': [(-1,0), (0, 1)],
+    'J': [(-1,0), (0,-1)]    
+}
+ADJ = {k: [_(x) for x in v] for k, v in ADJ.items()}
+ALL = ADJ['-'] + ADJ['|']
 
 def part1(f):
-    input = [[int(x) for x in n.split()] for n in f.split('\n') if n.strip()]
-    return sum(process(i) for i in input)
+    # We want to create a dict of each coordinate pair
+    arr = f.splitlines()
+    arr = {_((i, j)): s for i, line in enumerate(arr)
+           for j, s in enumerate(line)}
+    s = next(k for k, v in arr.items() if v == 'S')
+    s_adj = [dp for dp in ALL if -dp in ADJ[arr.get(s + dp, '.')]]
+    arr[s] = next(k for k, v in ADJ.items() if set(v) == set(s_adj))
+              
+    bfs = deque([(s, 0)])
+    dists = {}
+    while bfs:
+        p, d = bfs.popleft()
+        if p not in arr or p in dists:
+            continue
+        dists[p] = d
+        for dp in ADJ[arr[p]]:
+            bfs.append((p + dp, d + 1))
+    return max(dists.values()), dists, arr, s
+
 
 def part2(f):
-    input = [[int(x) for x in n.split()] for n in f.split('\n') if n.strip()]
-    return sum(process(i[::-1]) for i in input)
+    _, dists, arr, s = part1(f)
+    f = [list(s) for s in f.splitlines()]
+    f = [''.join(ch if (r,c) in dists else '.'
+        for c, ch in enumerate(row)) for r, row in enumerate(f)]
+    
+    outside = set()
+    for r, row in enumerate(f):
+        within = False
+        up = None
+        for c, ch in enumerate(row):
+            if (r, c) == s:
+                ch = arr[s]
+            if ch == '|':
+                assert up is None
+                within = not within
+            elif ch == '-':
+                assert up is not None
+            elif ch in 'LF':
+                assert up is None
+                up = ch == 'L'
+            elif ch in '7J':
+                assert up is not None
+                if ch != ('J' if up else '7'):
+                    within = not within
+                up = None
+            elif ch == '.':
+                pass
+            else:
+                raise RuntimeError(f'unexpected character: {ch}')
+            if not within:
+                outside.add((r,c))
+
+    return (len(f) * len(f[0]) - len(outside | dists.keys()))
+            
+
 
 
 def main():
---
+++ 9_year_2023/day11/solution.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env pypy3
+from itertools import combinations
+
+def _(tuple):
+
+
+    def __sub__(self, other):
+        return _(abs(x - y) for x,y in zip(self, other))
+
+
+def expand(f):
+    nf = []
+
+    for l in f:
+        if '#' in l:
+            nf += [l]
+        else:
+            nf += 2 * [l]
+    return nf
+
+def part1(f):
+
+
+    f = f.splitlines()
+    nf = expand(f)
+    nf = expand(zip(*nf))
+    coords = [(r, c) for r, row in enumerate(nf)
+             for c, ch in enumerate(row) if ch == '#']
+
+    combs = combinations(coords, 2)
+    return sum([abs(x-i) + abs(y-j) for ((x,y), (i,j)) in combs])
+
+def expand_big(f):
+    return [i for i, l in enumerate(f) if '#' not in l]
+
+
+def part2(f):
+
+
+    f = f.splitlines()
+    rows = expand_big(f)
+    cols = expand_big(zip(*f))
+    print(f'{f}, {rows}, {cols}')
+    coords = [(r, c) for r, row in enumerate(f)
+             for c, ch in enumerate(row) if ch == '#']
+    combs = combinations(coords, 2)
+
+    def get_big_dist(a, b):
+        (x,y), (i,j) = a, b
+        small_dist = abs(x - i) + abs(y - j)
+        for row in rows:
+            if row in (range(x, i) if x < i else range(i, x)):
+                small_dist += 999999
+
+        for col in cols:
+            if col in (range(y, j) if y < j else range(j, y)):
+                small_dist += 999999
+        return small_dist
+ 
+    return sum([get_big_dist(x, y) for x,y in combs])
+
+
+
+def main():
+    # inputs_1 = open('input').read()
+    inputs_2 = open('input').read()
+    # print(f'part 1: {part1(inputs_1)}')
+    print(f'part 2: {part2(inputs_2)}')
+
+
+if __name__ == '__main__':
+    main()
