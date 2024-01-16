8301d82413bb488ed39db42f232cb0f2d9b08f89
kevinlmadison <coolklm121@gmail.com>
2024-01-16 10:12:30 -0500

    working on day 12

--- 9_year_2023/day12/solution.py
+++ 9_year_2023/day12/solution.py
@@ -1,68 +1,40 @@
 #!/usr/bin/env pypy3
 from itertools import combinations
 
-def _(tuple):
+def count(cfg, nums):
+    if cfg == '':
+        return 1 if nums == () else 0
+    if nums == ():
+        return 0 if '#' in cfg else 1
+    result = 0
+    if cfg[0] in '.?':
+        result += count(cfg[1:], nums)
+    if cfg[0] in '#?':
+        if nums[0] <= len(cfg) and '.' not in cfg[:nums[0]] \
+        and (nums[0] == len(cfg) or cfg[nums[0]] != '#'):
+            result += count(cfg[nums[0] + 1:], nums[1:])
 
-
-    def __sub__(self, other):
-        return _(abs(x - y) for x,y in zip(self, other))
-
-
-def expand(f):
-    nf = []
-
-    for l in f:
-        if '#' in l:
-            nf += [l]
-        else:
-            nf += 2 * [l]
-    return nf
+    return result
 
 def part1(f):
-
-
     f = f.splitlines()
-    nf = expand(f)
-    nf = expand(zip(*nf))
-    coords = [(r, c) for r, row in enumerate(nf)
-             for c, ch in enumerate(row) if ch == '#']
+    total = 0
+    for line in f:
+        cfg, nums = line.split()
+        nums = tuple(map(int, nums.split(',')))
+        total += count(cfg, nums)
 
-    combs = combinations(coords, 2)
-    return sum([abs(x-i) + abs(y-j) for ((x,y), (i,j)) in combs])
-
-def expand_big(f):
-    return [i for i, l in enumerate(f) if '#' not in l]
+    return total
 
 
+    
 def part2(f):
-
-
-    f = f.splitlines()
-    rows = expand_big(f)
-    cols = expand_big(zip(*f))
-    print(f'{f}, {rows}, {cols}')
-    coords = [(r, c) for r, row in enumerate(f)
-             for c, ch in enumerate(row) if ch == '#']
-    combs = combinations(coords, 2)
-
-    def get_big_dist(a, b):
-        (x,y), (i,j) = a, b
-        small_dist = abs(x - i) + abs(y - j)
-        for row in rows:
-            if row in (range(x, i) if x < i else range(i, x)):
-                small_dist += 999999
-
-        for col in cols:
-            if col in (range(y, j) if y < j else range(j, y)):
-                small_dist += 999999
-        return small_dist
- 
-    return sum([get_big_dist(x, y) for x,y in combs])
+    ...
 
 
 
 def main():
-    inputs_1 = open('test_input').read()
+    inputs_1 = open('input').read()
     # inputs_2 = open('input').read()
     print(f'part 1: {part1(inputs_1)}')
     # print(f'part 2: {part2(inputs_2)}')
